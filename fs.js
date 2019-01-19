26bcf4475375a26a049cdcf22f08b99686960dd9
Eamonn de Leastar <edeleastar@wit.ie>
2019-01-19 07:37:01 +0000

    encapsulate and paramaterise feature into loadVenues() function

--- fs.js
+++ fs.js
@@ -9,17 +9,22 @@ const fsConfig = {
 
 const fsCredentials = '&client_id=' + fsConfig.client_id + '&client_secret=' + fsConfig.client_secret + '&v=20190101';
 
-console.log(fsConfig.base_url + 'near=Waterford,IE' + fsCredentials);
+function loadVenues(location) {
+  console.log(`Searching ${location}...`);
+  axios.get(fsConfig.base_url + location + fsCredentials).then(function(response) {
+    var venues = response.data.response.groups[0].items;
+    const venueCollection = [];
+    for (let i = 0; i < venues.length; i++) {
+      const singleVenue = venues[i].venue;
+      const venue = {
+        name: singleVenue.name
+      };
+      venueCollection.push(venue);
+    }
+    console.log(venueCollection);
+  });
+}
 
-axios.get(fsConfig.base_url + 'near=Waterford,IE' + fsCredentials).then(function(response) {
-  var venues = response.data.response.groups[0].items;
-  const venueCollection = [];
-  for (let i = 0; i < venues.length; i++) {
-    const singleVenue = venues[i].venue;
-    const venue = {
-      name: singleVenue.name,
-    };
-    venueCollection.push(venue);
-  }
-  console.log(venueCollection);
-});
+loadVenues('near=Waterford,IE');
+loadVenues('near=Tramore,IE');
+loadVenues('near=Dungarvan,IE');
